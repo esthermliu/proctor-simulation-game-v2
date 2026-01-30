2dc1afabf28264d183812529ccc8cafbe4c0a03d
Esther Liu <estliu@cs.washington.edu>
2026-01-30 04:08:51 -0800

    Scenes 0 and 1: Added black fade in and out

--- Assets/Scripts/Scene0Scripts/TitleScreenManager.cs
+++ Assets/Scripts/Scene0Scripts/TitleScreenManager.cs
@@ -6,6 +6,7 @@ public class TitleScreenManager : MonoBehaviour
     // This function will be called when the button is clicked
     public void PlayGame()
     {
-        SceneManager.LoadScene("Scene1_Day1_V2"); // Name of your next scene
+        FadeManager.Instance.FadeToScene("Scene1_Day1_V2");
+        //SceneManager.LoadScene("Scene1_Day1_V2"); // Name of your next scene
     }
 }
---
+++ Assets/Scripts/Scene1Scripts/V2Scripts/SceneFadeIn.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+
+public class SceneFadeIn : MonoBehaviour
+{
+    public CanvasGroup fadeCanvasGroup;
+    public float fadeDuration = 1f;
+
+    void Start()
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    IEnumerator FadeIn()
+    {
+        float t = 0f;
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            fadeCanvasGroup.alpha = Mathf.Lerp(1f, 0f, t / fadeDuration);
+            yield return null;
+        }
+
+        fadeCanvasGroup.alpha = 0f;
+        fadeCanvasGroup.blocksRaycasts = false; // allow clicks after fade
+    }
+}
