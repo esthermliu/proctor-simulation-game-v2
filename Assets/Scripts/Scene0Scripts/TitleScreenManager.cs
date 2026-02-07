9049d95db6add27eed218c61fb3c642efb50c052
Esther Liu <estliu@cs.washington.edu>
2026-02-07 15:58:39 -0800

    Scene 0: Added Controls and Credits pages

--- Assets/Scripts/Scene0Scripts/TitleScreenManager.cs
+++ Assets/Scripts/Scene0Scripts/TitleScreenManager.cs
@@ -3,10 +3,140 @@ using UnityEngine.SceneManagement; // Needed to load scenes
 
 public class TitleScreenManager : MonoBehaviour
 {
+    [Header("Title References")]
+    public GameObject titlePage;
+    public GameObject titlePencilA;
+    public GameObject titlePencilB;
+    public GameObject titlePencilC;
+
+    [Header("Controls References")]
+    public GameObject controlsPage;
+    public GameObject optionAInfo;
+    public GameObject optionBInfo;
+    public GameObject optionCInfo;
+
+    public GameObject controlsPencilA;
+    public GameObject controlsPencilB;
+    public GameObject controlsPencilC;
+
+    public GameObject controlsPaperEdge;
+
+    [Header("Credits References")]
+    public GameObject creditsPage;
+    public GameObject creditsPaperEdge;
+
     // This function will be called when the button is clicked
     public void PlayGame()
     {
         FadeManager.Instance.FadeToScene("Scene1_Day1_V2");
-        //SceneManager.LoadScene("Scene1_Day1_V2"); // Name of your next scene
     }
+
+    // ========================================
+    //          NAVIGATION FUNCTIONS
+    // ========================================
+
+    // This function is called when the edge of the page is clicked for both
+    // the CONTROLS and the CREDITS scene
+    public void ReturnToTitle()
+    {
+        // Reset all Controls Pencil Hoverlays (doesn't matter if we are actually on controls scene)
+        // AND any selected options
+        ResetControls();
+        ResetCredits();
+
+        // Set all other "pages" (i.e., CREDITS and CONTROLS) to inactive
+        controlsPage.SetActive(false);
+        creditsPage.SetActive(false);
+
+        // show the title screen page
+        titlePage.SetActive(true);
+    }
+
+    // Called on click to Controls from title screen
+    public void GoToControls()
+    {
+        // Make sure all pencil hoverlays are set as inactive in title screen first!
+        ResetTitle();
+
+        // Then set all other pages to inactive
+        titlePage.SetActive(false);
+        creditsPage.SetActive(false);
+
+        controlsPage.SetActive(true);
+    }
+
+    // Called on click to Credits from title screen
+    public void GoToCredits()
+    {
+        // Make sure all pencil hoverlays are set as inactive in title screen first!
+        ResetTitle();
+
+        titlePage.SetActive(false);
+        controlsPage.SetActive(false);
+
+        creditsPage.SetActive(true);
+    }
+
+
+    private void ResetTitle()
+    {
+        // reset the pencil hoverlays
+        titlePencilA.SetActive(false);
+        titlePencilB.SetActive(false);
+        titlePencilC.SetActive(false);
+    }
+
+    private void ResetControls()
+    {
+        // reset the pencil hoverlays
+        controlsPencilA.SetActive(false);
+        controlsPencilB.SetActive(false);
+        controlsPencilC.SetActive(false);
+
+        // reset the options shown
+        optionAInfo.SetActive(false);
+        optionBInfo.SetActive(false);
+        optionCInfo.SetActive(false);
+
+        // reset the edge of the paper
+        controlsPaperEdge.SetActive(false);
+    }
+
+    private void ResetCredits()
+    {
+        // reset the edge of the paper
+        creditsPaperEdge.SetActive(false);
+    }
+
+    // ========================================
+    //          "CONTROLS" FUNCTIONS
+    // ========================================
+    public void OnControlOptionClick(int optionNum) {
+        if (optionNum == 1) {
+            // Set all other option info to inactive
+            optionBInfo.SetActive(false);
+            optionCInfo.SetActive(false);
+
+            // Show option A info
+            optionAInfo.SetActive(true);
+        } else if (optionNum == 2)
+        {
+            // Set all other option info to inactive
+            optionAInfo.SetActive(false);
+            optionCInfo.SetActive(false);
+
+            // Show option B info
+            optionBInfo.SetActive(true);
+        } else if (optionNum == 3)
+        {
+            // Set all other option info to inactive
+            optionAInfo.SetActive(false);
+            optionBInfo.SetActive(false);
+
+            // Show option C info
+            optionCInfo.SetActive(true);
+        } 
+    }
+
+
 }
--- Assets/Scripts/Scene3Scripts/V2Scripts/SupervisorTalk.cs
+++ Assets/Scripts/Scene3Scripts/V2Scripts/SupervisorTalk.cs
@@ -4,17 +4,6 @@ using TMPro;
 
 public class SupervisorTalk : MonoBehaviour, IPointerClickHandler
 {
-    //public GameObject evalSprite;
-    //public GameObject endDay;
-
-    //[Header("Performance Evaluation Info")]
-    //public int dayNumber = 1;
-    //public TMP_Text correctAdmissionsText;
-    //public TMP_Text studentsFlaggedText;
-    //public TMP_Text studentsReportedText;
-    //public TMP_Text studentsHelpedText;
-    //public TMP_Text overallAssessmentText;
-
     [Header("Supervisor")]
     public Animator supervisorAnimator;
 
@@ -39,69 +28,5 @@ public class SupervisorTalk : MonoBehaviour, IPointerClickHandler
         // Since we started talking to the supervisor, trigger the start looking animation
         supervisorAnimator.SetTrigger("StartLooking");
 
-        // TODO: ONLY SHOW THE FORM AFTER THE DIALOGUE ENDS
-        // show the form
-        //evalSprite.SetActive(true);
-
-        //// show end day sprite
-        //endDay.SetActive(true);
-
-        //// Fill in the text from GameManager
-        //UpdatePerformanceEvalText();
-
     }
-
-    //void UpdatePerformanceEvalText()
-    //{
-    //    // Do not update Paycheck text if no game manager is present
-    //    if (GameManager.Instance == null)
-    //    {
-    //        return;
-    //    }
-
-    //    int correctToday = GameManager.Instance.correctToday;
-    //    int flaggedToday = GameManager.Instance.flaggedToday;
-    //    int reportedToday = GameManager.Instance.reportedToday;
-    //    int helpedToday = GameManager.Instance.helpedToday;
-
-
-    //    // update text
-    //    correctAdmissionsText.text = correctToday + "";
-    //    studentsFlaggedText.text = flaggedToday + "";
-    //    studentsReportedText.text = reportedToday + "";
-    //    studentsHelpedText.text = helpedToday + "";
-
-    //    overallAssessmentText.text = OverallAssessment();
-    //}
-
-    //private string OverallAssessment()
-    //{
-    //    if (dayNumber == 1)
-    //    {
-    //        return OverallAssessmentDay1();
-    //    }
-
-    //    return "";
-    //}
-
-    //private string OverallAssessmentDay1()
-    //{
-    //    int correctToday = GameManager.Instance.correctToday;
-    //    int flaggedToday = GameManager.Instance.flaggedToday;
-    //    int reportedToday = GameManager.Instance.reportedToday;
-    //    int helpedToday = GameManager.Instance.helpedToday;
-
-    //    bool satisfactory = correctToday >= 4 && flaggedToday >= 1 && reportedToday == 1 && helpedToday == 1;
-    //    bool requiresReview = correctToday >= 3 && flaggedToday >= 1 && reportedToday < 2 && helpedToday == 1;
-    //    if (satisfactory)
-    //    {
-    //        return "SATISFACTORY";
-    //    } else if (requiresReview)
-    //    {
-    //        return "REQUIRES REVIEW";
-    //    } else
-    //    {
-    //        return "INADEQUATE";
-    //    }
-    //}
 }
