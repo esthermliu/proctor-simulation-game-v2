72de57961b22cae6d616288c6ef3c74a51ab4ae6
Joshua Chen <josh@chenhome.org>
2026-02-20 16:21:39 -0800

    clickable hover effect

--- Assets/Scripts/Scene1Scripts/V2Scripts/Clickable.cs
+++ Assets/Scripts/Scene1Scripts/V2Scripts/Clickable.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class Clickable : MonoBehaviour, IPointerDownHandler
+public class Clickable : MonoBehaviour, IPointerDownHandler, 
+IPointerEnterHandler, IPointerExitHandler
 {
     public GameObject enlargedPaper;
+    public Color hoverColor = new Color(0.7f, 0.7f, 0.7f, 1f);
+
+    private Image image; 
 
     void Start()
     {
@@ -12,12 +17,18 @@ public class Clickable : MonoBehaviour, IPointerDownHandler
         {
             enlargedPaper.SetActive(false);
         }
+
+        image = GetComponent<Image>();
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        // 1) Hide the small version of the paper
+        // 1) Hide the small version of the paper, reset color
         gameObject.SetActive(false);
+        if (image != null)
+        {
+            image.color = Color.white; // reset to original color
+        }
 
 
         // 2) Show the enlarged version of the paper IF it exists
@@ -30,6 +41,22 @@ public class Clickable : MonoBehaviour, IPointerDownHandler
         } else
         {
             Debug.Log("ERROR (Clickable.cs): Missing enlarged version of paper");
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (image != null)
+        {
+            image.color = hoverColor;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (image != null)
+        {
+            image.color = Color.white; // reset to original color
         }
     }
 }
